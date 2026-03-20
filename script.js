f7b603888f9528dae3c4bbe567d0ae6cff80776a
niedjaraujos <naraujos1007@hotmail.com>
2026-03-20 15:26:30 -0300

    feat: add scroll para o header no arquivo js

---
+++ script.js
@@ -0,0 +1,9 @@
+//Mostrar backgroud do header ao scrollar a pág
+window.addEventListener("scroll", () => {
+  const scrollBar = document.querySelector("header");
+  if (window.scrollY > 50) {
+    scrollBar.classList.add("scrolled");
+  } else {
+    scrollBar.classList.remove("scrolled");
+  }
+});
